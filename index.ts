43aedc082819c0dad2bb17150f0ea8402e53d3bf
Alex Lindeman <hello@ael.me>
2020-04-12 17:02:03 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import type { Compiler, Plugin } from 'webpack';
+
+class TidyPlugin implements Plugin {
+  public apply = (compiler: Compiler): void => {
+    const logger = compiler.getInfrastructureLogger('TidyPlugin');
+    compiler.hooks.emit.tap('TidyPlugin', ({ assets }) => {
+      const tidyDir = path.resolve(compiler.outputPath);
+      fs.readdir(tidyDir, (readdirErr, files) => {
+        if (readdirErr) {
+          logger.warn('Could not tidy directory', tidyDir, readdirErr);
+        } else {
+          files.filter(file => !assets[file]).forEach(file => {
+            fs.unlink(path.resolve(tidyDir, file), unlinkErr => {
+              if (unlinkErr) {
+                logger.warn('Could not delete asset', file, unlinkErr);
+              } else {
+                logger.log('Deleted asset', file);
+              }
+            });
+          });
+        }
+      });
+    });
+  }
+}
+
+export default TidyPlugin;
